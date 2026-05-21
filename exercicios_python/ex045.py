d5b2956c560de78033a860db703c6bf3c86153e5
fernandopedrox <fernando.fefeu005@gmail.com>
2026-05-21 16:48:27 -0300

    atualizando o jogo com um temporizador

--- exercicios_python/ex045.py
+++ exercicios_python/ex045.py
@@ -1,4 +1,5 @@
 from random import choice
+from time import sleep
 while True:
     escolhas = ['pedra', 'papel', 'tesoura']
     bot = choice(escolhas)
@@ -6,6 +7,11 @@ while True:
     if jogador not in escolhas:
         print('Escolha inválida, tente novamente.')
     else:
+        print('JO')
+        sleep(0.5)
+        print('KEN')
+        sleep(0.5)
+        print('PO!!!')
         if jogador == bot:
             print(f'Empate! O computador escolheu {bot.upper()} e o jogador escolheu {jogador.upper()}')
         elif jogador == 'pedra' and bot == 'tesoura' or jogador == 'papel' and bot == 'pedra' or jogador == 'tesoura' and bot == 'papel':
